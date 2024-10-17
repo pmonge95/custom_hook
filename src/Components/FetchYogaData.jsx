9c5cd55faa337ed65dc00fb7b632ba8cac5aeff7
pmonge95 <pmonge95@gmail.com>
2024-10-17 12:19:43 -0400

    Added the code for fetching data

--- src/Components/FetchYogaData.jsx
+++ src/Components/FetchYogaData.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import UseFetch from './UseFetch';
 import './FetchData.css'
 
-const FetchData = () => {
-    const [data]=UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
+const FetchYogaData = () => {
+    const [data]=UseFetch('https://api.npoint.io/9045c260b1565daa9e15');
     console.log(data);
     return (
     <>
@@ -14,13 +14,12 @@ const FetchData = () => {
                     <h3>{e.name}</h3>
                     <p><strong>Importance: </strong>{e.importance}</p>
                     <p><strong>Benefits: </strong>{e.benefits}</p>
-                    <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+                    <p><strong>Duration: </strong>{e.time_duration}</p>
                 </li>
                 </>
             ))}
         </ul>
     </>
-  )
+    )
 }
-
-export default FetchData
+export default FetchYogaData
